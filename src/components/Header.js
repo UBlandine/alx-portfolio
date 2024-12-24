545b590d80a858a94db8199e3bf0f276379f8a7b
UBlandine <124615325+UBlandine@users.noreply.github.com>
2024-12-24 16:52:08 +0200

    Home page user I

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+// import "./Header.css"; 
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <h1 className="logo">DevFolio</h1>
+        <ul className="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li>
+            <a href="#dropdown">Dropdown</a>
+            <ul className="dropdown">
+              <li><a href="#option1">Option 1</a></li>
+              <li><a href="#option2">Option 2</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./Home.css"; 
+
+const Home = () => {
+  return (
+    <div className="home" id="home">
+      <div className="content">
+        <h1>I am <span>Morgan Freeman</span></h1>
+        <p>Designer</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
