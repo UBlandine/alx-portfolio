67a0871f76a7cfe76f676a697eaf576c6dfc21da
UBlandine <124615325+UBlandine@users.noreply.github.com>
2025-01-08 01:03:22 +0200

    All to day

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,25 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link for React Router
 import "./Home.css";
- 
 
 const Header = () => {
   return (
     <header>
       <nav className="navbar">
-        <h1 className="logo">Blafolio</h1>
         <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-        
-          <li>
-            <a href="#dropdown">Dropdown</a>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/services">Services</Link></li>
+
+          <li className="dropdown-container">
+            <Link to="#dropdown" className="dropdown-toggle">Dropdown</Link>
             <ul className="dropdown">
-              <li><a href="#option1">Option 1</a></li>
-              <li><a href="#option2">Option 2</a></li>
+              <li><Link to="#option1">Option 1</Link></li>
+              <li><Link to="#option2">Option 2</Link></li>
             </ul>
           </li>
-          <li><a href="#contact">Contact</a></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
